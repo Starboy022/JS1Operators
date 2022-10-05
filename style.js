5f7bea4e94912f742949b267318cf95f3bb8afe0
Starboy <109226269+Starboy022@users.noreply.github.com>
2022-10-05 12:02:26 +0530

    all operators and it's example

---
+++ style.js
@@ -0,0 +1,38 @@
+//Arithemetic Operators
+let a=50,b=10;
+console.log("Arithemetic Operators");
+console.log("a+b: "+ (a+b));
+console.log("a-b: "+ (a-b));
+console.log("a*b: "+ (a*b));
+console.log("a/b: "+ (a/b));
+console.log("a%b: "+ (a%b));
+//Relational Operators
+let c=10,d=20;
+console.log("Relational Operators");
+console.log("c==d?: "+(c===d));
+console.log("c!=d?: "+(c!==d));
+console.log("c<d?: "+(c<d));
+console.log("c>d?: "+(c>d));
+console.log("c<=d?: "+(c<=d));
+console.log("c>=d?: "+(c>=d));
+
+//logical Operators
+let x=10,y=20;
+console.log("Logical Operators");
+console.log("(x<y)&&(y>x): "+((x<y)&&(y>x)));
+console.log("(x>y)||(y>x): "+((x>y)||(y>x)));
+
+let e=10;
+let f=5;
+e += 2;
+f *= 3;
+console.log(e)
+console.log(f)
+
+let j=2;
+console.log(++j)
+let k=16;
+console.log(--k)
+
+let siz=154;
+console.log(typeof(siz))
